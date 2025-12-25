b60777aa74e71ed159b952efef7e88415300191f
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-12-24 20:27:25 -0600

    :sparkles: Add categories and publishers endpoints

--- projects/02-books-api/main.go
+++ projects/02-books-api/main.go
@@ -35,13 +35,25 @@ func main() {
 	authorService := services.NewAuthorService(authorStore)
 	authorHandler := transport.NewAuthorHandler(authorService)
 
+	categoryStore := store.NewCategoryStore(db)
+	categoryService := services.NewCategoryService(categoryStore)
+	categoryHandler := transport.NewCategoryHandler(categoryService)
+
 	configStore := store.NewConfigurationStore(db)
 	configService := services.NewConfigurationService(configStore)
 	configHandler := transport.NewConfigurationHandler(configService)
 
+	publisherStore := store.NewPublisherStore(db)
+	publisherService := services.NewPublisherService(publisherStore)
+	publisherHandler := transport.NewPublisherHandler(publisherService)
+
 	http.HandleFunc("/authors", authorHandler.HandleAuthors)
 	http.HandleFunc("/authors/", authorHandler.HandleAuthorByID)
+	http.HandleFunc("/categories", categoryHandler.HandleCategories)
+	http.HandleFunc("/categories/", categoryHandler.HandleCategoryByID)
 	http.HandleFunc("/configuration", configHandler.HandleConfiguration)
+	http.HandleFunc("/publishers", publisherHandler.HandlePublishers)
+	http.HandleFunc("/publishers/", publisherHandler.HandlePublisherByID)
 
 	fmt.Println("Servidor escuchando en el puerto 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
